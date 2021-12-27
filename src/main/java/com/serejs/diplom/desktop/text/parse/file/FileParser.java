a51dcdf5457fe753fdb4f09401756db28321945a
Sokolov Sergey <serejs@jindekara.com>
2021-12-27 16:52:04 +0300

    Метод для получения простого текста из файла

--- src/main/java/com/serejs/diplom/desktop/text/parse/file/FileParser.java
+++ src/main/java/com/serejs/diplom/desktop/text/parse/file/FileParser.java
@@ -1,4 +1,20 @@
 package com.serejs.diplom.desktop.text.parse.file;
 
+import java.io.*;
+import java.util.Objects;
+
 public class FileParser {
+    public static String getText(String uri) {
+        StringBuilder text = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(Objects.requireNonNull(FileParser.class.getClassLoader().getResource(uri)).getFile())
+        )) {
+            reader.lines().forEach(text::append);
+        } catch (IOException e) {
+            System.err.println("Ошибка получения текста из файла: " + uri);
+        }
+
+        return text.toString();
+    }
 }
