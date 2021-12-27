9564dfd9034472ec1fd2d4c0fef5945fbaf6c011
Sokolov Sergey <serejs@jindekara.com>
2021-12-27 18:21:55 +0300

    Преобразование в запись и создание методов формата литературы

--- src/main/java/com/serejs/diplom/desktop/text/container/Format.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Format.java
@@ -1,4 +1,21 @@
 package com.serejs.diplom.desktop.text.container;
 
-public class Format {
+import com.serejs.diplom.desktop.text.parse.file.LiteratureType;
+
+public record Format(LiteratureType type, String prev, String mid, String after) {
+    public LiteratureType getType() {
+        return type;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public String getAfter() {
+        return after;
+    }
 }
