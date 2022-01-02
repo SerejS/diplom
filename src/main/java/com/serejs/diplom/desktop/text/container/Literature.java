f89a3c56d020d857c472621b4b6e90dc02ba0b1c
Sokolov Sergey <serejs@jindekara.com>
2022-01-02 15:38:31 +0300

    Первичная литература

--- src/main/java/com/serejs/diplom/desktop/text/container/Literature.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Literature.java
@@ -1,13 +1,37 @@
 package com.serejs.diplom.desktop.text.container;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Literature {
-    private Map<String, String> fragments;
+    private final static int minLenFragment = 150;
+    private Map<String, String> fragments = new HashMap<>();
+    private boolean main = true;
 
     public Literature() {}
 
     public Literature(Map<String, String> fragments) {
+        setFragments(fragments);
+    }
+
+    public Literature(Map<String, String> fragments, boolean main) {
+        this.main = main;
+        setFragments(fragments);
+    }
+
+    private void setFragments(Map<String, String> fragments) {
+        fragments.keySet().forEach(key -> {
+            if (fragments.get(key).length() >= minLenFragment) {
+                this.fragments.put(key, fragments.get(key));
+            }
+        });
+    }
+
+    public void getFragments(Map<String, String> fragments) {
         this.fragments = fragments;
+    }
+
+    public boolean isMain() {
+        return main;
     }
 }
