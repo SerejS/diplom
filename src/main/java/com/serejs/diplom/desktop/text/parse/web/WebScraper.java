4ffa24f521c94190eed359c2cc51f69ffe54ab28
Sokolov Sergey <serejs@jindekara.com>
2022-01-02 14:10:51 +0300

    Первичная реализация получения текста с сайта

--- src/main/java/com/serejs/diplom/desktop/text/parse/web/WebScraper.java
+++ src/main/java/com/serejs/diplom/desktop/text/parse/web/WebScraper.java
@@ -1,4 +1,25 @@
 package com.serejs.diplom.desktop.text.parse.web;
 
-class WebScraper {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class WebScraper {
+    public static String textFromSite(String siteURL)  {
+        StringBuilder sb = new StringBuilder();
+
+        Document doc;
+        try {
+            doc = Jsoup.connect(siteURL).get();
+        } catch (IOException e) {
+            return "";
+        }
+        doc.getElementsByTag("p").forEach((element) -> {
+            if (element.text().length() > 100) {
+                sb.append(element.text()).append('\n');
+            }
+        });
+        return sb.toString();
+    }
 }
