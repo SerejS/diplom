34f909887cfd83e6d7d8c6c181eacb4b58c35379
Sokolov Sergey <serejs@jindekara.com>
2022-01-02 15:29:58 +0300

    Реализация класса для тем

--- src/main/java/com/serejs/diplom/desktop/text/container/Theme.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Theme.java
@@ -1,4 +1,22 @@
 package com.serejs.diplom.desktop.text.container;
 
-class Theme {
+import java.util.Set;
+
+public record Theme(Theme root, String title, byte percent, Set<String> keyWords) {
+
+    public Theme getRoot() {
+        return root;
+    }
+
+    public byte getPercent() {
+        return percent;
+    }
+
+    public Set<String> getKeyWords() {
+        return keyWords;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
