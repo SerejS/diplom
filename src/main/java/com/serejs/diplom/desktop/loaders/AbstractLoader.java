bbdfb9c743dae31c64594f3d43cea9aa3ff4cbd3
Sokolov Sergey <serejs@jindekara.com>
2022-03-24 20:45:45 +0300

    Решение NullPointer Attachments

--- src/main/java/com/serejs/diplom/desktop/loaders/AbstractLoader.java
+++ src/main/java/com/serejs/diplom/desktop/loaders/AbstractLoader.java
@@ -2,13 +2,14 @@ package com.serejs.diplom.desktop.loaders;
 
 import com.serejs.diplom.desktop.text.container.Attachment;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractLoader implements ContentLoader {
-    protected Map<String, String> fragments;
-    protected Map<String, Set<Attachment>> attachments;
+    protected Map<String, String> fragments = new HashMap<>();
+    protected Map<String, Set<Attachment>> attachments = new HashMap<>();
 
     @Override
     public Set<Attachment> getAttachments(String key) {
