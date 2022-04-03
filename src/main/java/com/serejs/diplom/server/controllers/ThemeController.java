b72c7c658e3030c780969b74bc641dc6b1246711
Sokolov Sergey <serejs@jindekara.com>
2022-04-03 10:03:44 +0300

    Получение тем по идетификатору проекта

--- src/main/java/com/serejs/diplom/server/controllers/ThemeController.java
+++ src/main/java/com/serejs/diplom/server/controllers/ThemeController.java
@@ -3,6 +3,7 @@ package com.serejs.diplom.server.controllers;
 import com.serejs.diplom.server.entities.Theme;
 import com.serejs.diplom.server.repositories.ThemeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +16,9 @@ public class ThemeController {
         this.repository = repository;
     }
 
-    @GetMapping("/themes")
-    public List<Theme> getThemes() {
-        return repository.findAll();
+    @GetMapping("/themes/{id}")
+    public List<Theme> getThemes(@PathVariable Long id) {
+        return repository.findThemesByProjectId(id);
     }
 
 }
--- src/main/java/com/serejs/diplom/server/repositories/ThemeRepository.java
+++ src/main/java/com/serejs/diplom/server/repositories/ThemeRepository.java
@@ -2,6 +2,19 @@ package com.serejs.diplom.server.repositories;
 
 import com.serejs.diplom.server.entities.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+    @Query(value = """
+        SELECT t
+        FROM Theme t
+        WHERE t.project.id=:id_project
+    """)
+    List<Theme> findThemesByProjectId(@Param("id_project") Long projectId);
 }
