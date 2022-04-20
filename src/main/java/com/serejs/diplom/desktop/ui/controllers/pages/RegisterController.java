98bad11ab7344c023260d0410571cc682f9ac56d
jindekara <serejs@jindekara.com>
2022-04-20 13:58:49 +0300

    Контроллер регистрации отправляет данные

--- src/main/java/com/serejs/diplom/desktop/ui/controllers/pages/RegisterController.java
+++ src/main/java/com/serejs/diplom/desktop/ui/controllers/pages/RegisterController.java
@@ -1,13 +1,15 @@
 package com.serejs.diplom.desktop.ui.controllers.pages;
 
+import com.serejs.diplom.desktop.server.ServerClient;
 import com.serejs.diplom.desktop.ui.alerts.ErrorAlert;
 import com.serejs.diplom.desktop.ui.alerts.InfoAlert;
 import com.serejs.diplom.desktop.ui.controllers.abstracts.RootController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apache.http.HttpException;
 
-import static com.serejs.diplom.desktop.utils.Encrypt.encrypt;
+import java.io.IOException;
 
 public class RegisterController extends RootController {
     @FXML
@@ -29,32 +31,26 @@ public class RegisterController extends RootController {
 
     @FXML
     private void register() {
-        var login = this.login.getText();
-        var pass = this.pass.getText();
+        var username = this.login.getText();
+        var password = this.pass.getText();
         var repeat = this.repeat.getText();
 
         var minLength = 5;
-        if (login.length() < minLength || pass.length() < minLength) {
+        if (username.length() < minLength || password.length() < minLength) {
             ErrorAlert.info("Поля должны содержать как минимум 5 символов!");
             return;
         }
 
-        if (!pass.equals(repeat)) {
+        if (!password.equals(repeat)) {
             ErrorAlert.info("Пароли должны совпадать!");
             return;
         }
 
-        String salt = null;
         try {
-            var encrypted = encrypt(pass);
-            pass = encrypted.getKey();
-            salt = encrypted.getValue();
-        } catch (Exception e) {
-            ErrorAlert.info("Ошибка шифрования пароля");
-            return;
+            ServerClient.register(username, password);
+        } catch (IOException | HttpException e) {
+            e.printStackTrace();
         }
-
-        ///Тут должен быть запрос к серверу...
 
         InfoAlert.info("Аккаунт успешно создан");
         anotherPage(regButton, "login-view.fxml");
