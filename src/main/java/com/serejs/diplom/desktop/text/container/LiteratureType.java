6bd70047dd23d0fb9a37a37266cfb4cc60706697
jindekara <serejs@jindekara.com>
2022-04-20 14:07:42 +0300

    Добавление поля идентификатора в класс типа литературы

--- src/main/java/com/serejs/diplom/desktop/text/container/LiteratureType.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/LiteratureType.java
@@ -6,8 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LiteratureType {
+    private Long id;
     private final String title;
     private final boolean main;
+
+    public LiteratureType(String title, boolean main) {
+        this.id = -1L;
+        this.title = title;
+        this.main = main;
+    }
 
     @Override
     public String toString() {
