f7b7017526d83c37dffb00d6810a725e6b1af2f3
jindekara <serejs@jindekara.com>
2022-04-20 13:57:01 +0300

    Использование Lombok в классе View

--- src/main/java/com/serejs/diplom/desktop/text/container/View.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/View.java
@@ -1,22 +1,13 @@
 package com.serejs.diplom.desktop.text.container;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class View {
     private final Long id;
     private final String title;
-
-    public View(Long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
 
     @Override
     public String toString() {
