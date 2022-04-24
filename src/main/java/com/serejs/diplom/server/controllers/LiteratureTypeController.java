4703581867f27ab86599187ee9fce375525ec890
jindekara <serejs@jindekara.com>
2022-04-24 12:08:40 +0300

    Изменение контроллера типов литературы

--- src/main/java/com/serejs/diplom/server/controllers/LiteratureTypeController.java
+++ src/main/java/com/serejs/diplom/server/controllers/LiteratureTypeController.java
@@ -2,12 +2,14 @@ package com.serejs.diplom.server.controllers;
 
 import com.serejs.diplom.server.entities.LiteratureType;
 import com.serejs.diplom.server.repositories.LiteratureTypeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/types")
 public class LiteratureTypeController {
     private final LiteratureTypeRepository repository;
 
@@ -15,13 +17,15 @@ public class LiteratureTypeController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/types")
+    @GetMapping
     public List<LiteratureType> getTypes(@RequestParam Long viewId) {
         return repository.findLiteratureTypeByViewId(viewId);
     }
 
-    @PostMapping(value = "/types")
-    public void addType(@RequestBody LiteratureType type) {
+    @PostMapping
+    public ResponseEntity<LiteratureType> createType(@RequestBody LiteratureType type) {
         repository.save(type);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
