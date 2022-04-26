e818471fda9f3260631e00b89bc9e48a353cacbe
jindekara <serejs@jindekara.com>
2022-04-26 12:18:50 +0300

    Реализация JsonSerializable в Theme

--- src/main/java/com/serejs/diplom/desktop/text/container/Theme.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Theme.java
@@ -1,15 +1,20 @@
 package com.serejs.diplom.desktop.text.container;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.serejs.diplom.desktop.analyze.Analyzer;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import java.util.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Data
 @EqualsAndHashCode
 //Percent относительно root -> фактический
-public class Theme {
+public class Theme implements JsonSerializable {
     private Long id;
     private Theme root;
     private String title;
@@ -18,12 +23,15 @@ public class Theme {
     private Set<String> keyNGrams;
     private Set<LiteratureType> types;
 
-    public Theme(Theme root, String title, double percent, String textKeyNGrams, Set<LiteratureType> types) {
+    private Project project;
+
+    public Theme(Theme root, Project project, String title, double percent, String textKeyNGrams, Set<LiteratureType> types) {
         this.root = root;
         this.title = title;
         this.textKeyNGrams = textKeyNGrams;
         this.keyNGrams = new HashSet<>(Analyzer.parseNGrams(textKeyNGrams));
         this.types = types;
+        this.project = project;
 
         if (this.root != null) {
             this.types.addAll(root.types);
@@ -35,8 +43,8 @@ public class Theme {
     }
 
 
-    public Theme(Long id, Theme root, String title, double percent, String textKeyNGrams, Set<LiteratureType> types) {
-        this(root, title, percent, textKeyNGrams, types);
+    public Theme(Long id, Project project, Theme root, String title, double percent, String textKeyNGrams, Set<LiteratureType> types) {
+        this(root, project, title, percent, textKeyNGrams, types);
         this.id = id;
     }
 
@@ -46,19 +54,33 @@ public class Theme {
         if (root != null) this.keyNGrams.addAll(root.keyNGrams);
     }
 
-    public static List<Theme> getLeafThemes(List<Theme> themes) {
-        ArrayDeque<Theme> deque = new ArrayDeque<>(themes);
-        ArrayDeque<Theme> childDeque = new ArrayDeque<>();
-        while (!deque.isEmpty()) {
-            Theme theme = deque.pop();
-            List<Theme> subThemes = deque.stream().filter(t -> t.getRoot() == theme).toList();
-            if (subThemes.size() > 0) {
-                subThemes.forEach(deque::push);
-            } else {
-                childDeque.push(theme);
+    @Override
+    public JsonObject toJson() {
+        Gson gson = new Gson();
+        var themeObj = gson.toJsonTree(this).getAsJsonObject();
+        themeObj.remove("project");
+
+        JsonObject nestedJson = null;
+        if (project != null) {
+            nestedJson = new JsonObject();
+            nestedJson.addProperty("id", project.getId());
+        }
+        themeObj.add("project", nestedJson);
+
+        //Замена объектов типов литературы на их идентификаторы
+        JsonArray nestedJsonArray = null;
+        if (types != null) {
+            nestedJsonArray = new JsonArray();
+
+            for (var type : types) {
+                var typeObject = new JsonObject();
+                typeObject.addProperty("id", type.getId());
+                nestedJsonArray.add(typeObject);
             }
         }
-        return childDeque.stream().distinct().toList();
+        themeObj.add("types", nestedJsonArray);
+
+        return themeObj;
     }
 
     @Override
