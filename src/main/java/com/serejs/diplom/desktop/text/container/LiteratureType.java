01c7bcd3fa8e59d3c2e5e8a8a73ad6c0cd768192
jindekara <serejs@jindekara.com>
2022-04-26 12:18:08 +0300

    Реализация JsonSerializable в LiteratureType

--- src/main/java/com/serejs/diplom/desktop/text/container/LiteratureType.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/LiteratureType.java
@@ -1,12 +1,14 @@
 package com.serejs.diplom.desktop.text.container;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.serejs.diplom.desktop.ui.states.State;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class LiteratureType {
+public class LiteratureType implements JsonSerializable {
     private Long id;
     private final String title;
     private final boolean main;
@@ -29,5 +31,22 @@ public class LiteratureType {
     @Override
     public String toString() {
         return title;
+    }
+
+    @Override
+    public JsonObject toJson() {
+        Gson gson = new Gson();
+        var typeObj = gson.toJsonTree(this).getAsJsonObject();
+        typeObj.remove("view");
+
+        JsonObject nestedJson = null;
+        if (view != null) {
+            nestedJson = new JsonObject();
+            nestedJson.addProperty("id", view.getId());
+        }
+
+        typeObj.add("view", nestedJson);
+
+        return typeObj;
     }
 }
