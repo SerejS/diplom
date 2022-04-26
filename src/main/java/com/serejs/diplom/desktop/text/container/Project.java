bdde3592df7cf2e5d0c2ef4c6e8b5577193c6b98
jindekara <serejs@jindekara.com>
2022-04-26 12:18:31 +0300

    Реализация JsonSerializable в Project

--- src/main/java/com/serejs/diplom/desktop/text/container/Project.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Project.java
@@ -1,14 +1,33 @@
 package com.serejs.diplom.desktop.text.container;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class Project {
+public class Project implements JsonSerializable {
     private Long id;
     private String title;
     private View view;
+
+    @Override
+    public JsonObject toJson() {
+        Gson gson = new Gson();
+        var projectObj = gson.toJsonTree(this).getAsJsonObject();
+        projectObj.remove("view");
+
+        JsonObject nestedJson = null;
+        if (view != null) {
+            nestedJson = new JsonObject();
+            nestedJson.addProperty("id", view.getId());
+        }
+
+        projectObj.add("view", nestedJson);
+
+        return projectObj;
+    }
 
     @Override
     public String toString() {
