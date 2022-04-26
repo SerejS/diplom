8af717a46141f940b9c12314c359d89cc7f530f5
jindekara <serejs@jindekara.com>
2022-04-26 12:19:00 +0300

    Реализация JsonSerializable в View

--- src/main/java/com/serejs/diplom/desktop/text/container/View.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/View.java
@@ -1,16 +1,24 @@
 package com.serejs.diplom.desktop.text.container;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class View {
+public class View implements JsonSerializable {
     private Long id;
     private final String title;
 
     @Override
     public String toString() {
         return title;
+    }
+
+    @Override
+    public JsonObject toJson() {
+        var gson = new Gson();
+        return gson.toJsonTree(this).getAsJsonObject();
     }
 }
