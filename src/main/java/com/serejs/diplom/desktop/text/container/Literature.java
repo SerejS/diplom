50b549d9fcfa5e4673c99472b5a10f04654037e3
Sokolov Sergey <serejs@jindekara.com>
2022-05-17 10:18:04 +0300

    Исправление функции сериализации Literature в json для корректного запроса к серверу

--- src/main/java/com/serejs/diplom/desktop/text/container/Literature.java
+++ src/main/java/com/serejs/diplom/desktop/text/container/Literature.java
@@ -1,11 +1,11 @@
 package com.serejs.diplom.desktop.text.container;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.serejs.diplom.desktop.enums.SourceType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.File;
 import java.net.URI;
 
 @Data
@@ -16,10 +16,29 @@ public class Literature implements JsonSerializable {
     private SourceType sourceType;
     private LiteratureType litType;
 
+    private Project project;
+
     @Override
     public JsonObject toJson() {
-        var gson = new Gson();
-        var element = gson.toJsonTree(this);
-        return element.getAsJsonObject();
+        var litObj = new JsonObject();
+
+        litObj.addProperty("source", sourceType.ordinal());
+        litObj.addProperty("path", new File(uri).getName());
+
+        JsonObject nestedJson = null;
+        if (litType != null) {
+            nestedJson = new JsonObject();
+            nestedJson.addProperty("id", litType.getId());
+        }
+        litObj.add("type", nestedJson);
+
+        nestedJson = null;
+        if (project != null) {
+            nestedJson = new JsonObject();
+            nestedJson.addProperty("id", project.getId());
+        }
+        litObj.add("project", nestedJson);
+
+        return litObj;
     }
 }
